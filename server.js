1caaf364a76ab7670c74f0c59eefedd748cde599
Daniel schwartz <mdblakestj@gmail.com>
2018-09-01 19:13:00 -0400

    added git ignore and handlebars

--- server.js
+++ server.js
@@ -1,26 +1,57 @@
 const express = require("express");
+const hbs = require('hbs');
+const fs = require('fs');
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// })
+
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.')
+    }
+  });
+  next();
+
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
 app.get('/',(req,res) =>{
-  // res.send('<h1>hello Express</h1>')
-  res.send({
-    name: 'Dan',
-    likes: [
-      "biking",
-      "cities"
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: "Hey Hey Hey!"
 
-
+  })
 
   })
 
 
-})
+
 
 app.get('/about', (req,res) => {
-  res.send('<h1> About Page </h1>')
+  res.render('about.hbs', {
+    pageTitle: "About Page",
+
+  });
 })
 
 app.get('/bad', (req,res) => {
